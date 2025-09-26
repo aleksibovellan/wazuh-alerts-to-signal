49555da17f8563fb6ce9f04cd97866c7fd5a09c5
Aleksi Bovellan <53447593+aleksibovellan@users.noreply.github.com>
2025-09-26 07:32:27 +0300

    Update refresh_token.sh

--- refresh_token.sh
+++ refresh_token.sh
@@ -8,6 +8,6 @@ API_URL="https://localhost:55000"
 # Generate token
 TOKEN=$(curl -sk -u $USER:$PASS -X POST "$API_URL/security/user/authenticate?raw=true")
 
-# Save token to file
+# Save token to file (check file path to match your system)
 echo "$TOKEN" > /home/user/wazuh_token.txt
 chmod 600 /home/user/wazuh_token.txt
