4a06e68be6ab89750506593ee0d30624cb3f2e2a
Aleksi Bovellan <53447593+aleksibovellan@users.noreply.github.com>
2025-12-04 05:13:22 +0200

    Fixed Signal link time-expiration with automatic refresh

--- refresh_token.sh
+++ refresh_token.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# WAZUH API TOKEN REFRESHER
 
-# Newly created Wazuh API user credentials
 USER="signalbot"
 PASS="SomethingStrong!"
 API_URL="https://localhost:55000"
 
-# Generate token
+# Adjust path to where you want the token stored
+OUTPUT_FILE="/home/user/wazuh_token.txt"
+
+# Request Token
 TOKEN=$(curl -sk -u $USER:$PASS -X POST "$API_URL/security/user/authenticate?raw=true")
 
-# Save token to file (check file path to match your system)
-echo "$TOKEN" > /home/user/wazuh_token.txt
-chmod 600 /home/user/wazuh_token.txt
+# Validate
+if [[ $TOKEN == eyJ* ]]; then
+    echo "$TOKEN" > "$OUTPUT_FILE"
+    chmod 600 "$OUTPUT_FILE"
+else
+    # Simple error logging (optional)
+    echo "Failed to get Wazuh Token"
+fi
